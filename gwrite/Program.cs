a9e064f592f33bdb98afd3175b9166678209a597
Jake Bladt <jake@jakebladt.com>
2018-11-01 16:19:00 -0400

    initial commit

---
+++ gwrite/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace gwrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var psi = new ProcessStartInfo("cmd", "/c git init") 
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute =  false,
+                CreateNoWindow = true,
+                WorkingDirectory = @"C:\jake"
+            };
+
+            var proc = new Process()
+            {
+                StartInfo = psi
+            };
+            proc.Start();
+
+            Console.WriteLine(proc.StandardOutput.ReadToEnd());
+            Console.ReadLine();
+        }
+    }
+}
